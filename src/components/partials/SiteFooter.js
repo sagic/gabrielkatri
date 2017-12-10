c328d1a8530161e3d57d76dcbbf760c971a502d6
Sagi Chizik <sagi.chizik@harrenmedia.com>
2017-12-10 14:36:50 +0200

    footer and menus cosmetics

--- src/components/partials/SiteFooter.js
+++ src/components/partials/SiteFooter.js
@@ -1,12 +1,37 @@
 import React from 'react';
-import FlagSeperator from './FlagSeperator';
+// import FlagSeperator from './FlagSeperator';
 import styles from './SiteFooter.module.scss';
 
 export default () => (
   <div className={styles.wrapper}>
-    <FlagSeperator />
-    <div>
-      Footer
+    {/* <FlagSeperator /> */}
+    <div className={styles.column}>
+      <h5>מידע כללי</h5>
+      <ul className={styles.menu}>
+        <li><a href="/parallax">פרופיל</a></li>
+        <li><a href="/about">אודותינו</a></li>
+        <li><a href="/contact">יצירת קשר</a></li>
+      </ul>
+    </div>
+    <div className={styles.column}>
+      <h5>כתבות</h5>
+      <ul className={styles.menu}>
+        <li><a href="/citizenship">אזרחות רומנית</a></li>
+        <li><a href="/birth-cert">תעודת לידה רומנית</a></li>
+        <li><a href="/passport">זכאות לדרכון רומני</a></li>
+        <li><a href="/advantages">יתרונות לדרכון רומני</a></li>
+        <li><a href="/real-estate">נדל״ן ברומניה</a></li>
+        <li><a href="/property">השבת רכוש</a></li>
+      </ul>
+    </div>
+    <div className={styles.column}>
+      <h5>משרדנו</h5>
+      <ul className={styles.menu}>
+        <li>סוקולוב 29, הרצליה</li>
+        <li>שד' המגינים 39, חיפה</li>
+        <li>טלפון: 09-9509504, 04-8525130</li>
+        <li>פקס: 09-9586332</li>
+      </ul>
     </div>
   </div>
 );
